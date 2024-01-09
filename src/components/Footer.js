f576d8f67fe107de09978a7917808c9f5b7c8fc5
Qaisar Khan <qaisarkhan234@gmail.com>
2024-01-09 17:10:24 +0500

    Added footer and fixes other bugs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,83 @@
-import React from 'react'
-import './Footer.css'
+import React, { useEffect, useState } from "react";
+import "./Footer.css";
 const Footer = () => {
-  return (
-    <div className='foot_panel1'>
-        <p>Back to top</p>
-        
-    </div>
-  )
-}
+  const [showButton, setShowButton] =  useState(false);
+  useEffect(()=>{
+    const handleScroll = () => {
+      if(window.pageYOffset > 300){
+        setShowButton(true)
+      }else{
+        setShowButton(false)
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
 
-export default Footer
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth" // Smooth scrolling animation
+    });
+  };
+  return (
+    <div className="footer">
+      <div className="foot_panel1">
+        <p onClick={scrollToTop} style={{cursor:"pointer"}}>Back to top</p>
+      </div>
+      <div className="foot_panel2">
+        <ul className="footer_ul">
+          <p>Get to Know Us</p>
+          <a>Careers</a>
+          <a>Blog</a>
+          <a>About Amazon</a>
+          <a>Investor Relations</a>
+          <a>Amazon Devices</a>
+          <a>Amazon Science</a>
+        </ul>
+        <ul className="footer_ul">
+          <p>Get to Know Us</p>
+          <a>Careers</a>
+          <a>Blog</a>
+          <a>About Amazon</a>
+          <a>Investor Relations</a>
+          <a>Amazon Devices</a>
+          <a>Amazon Science</a>
+        </ul>
+        <ul className="footer_ul">
+          <p>Get to Know Us</p>
+          <a>Careers</a>
+          <a>Blog</a>
+          <a>About Amazon</a>
+          <a>Investor Relations</a>
+          <a>Amazon Devices</a>
+          <a>Amazon Science</a>
+        </ul>
+        <ul className="footer_ul">
+          <p>Get to Know Us</p>
+          <a>Careers</a>
+          <a>Blog</a>
+          <a>About Amazon</a>
+          <a>Investor Relations</a>
+          <a>Amazon Devices</a>
+          <a>Amazon Science</a>
+        </ul>
+      </div>
+      <div className="foot_panel3">
+        <div className="logo"></div>
+      </div>
+      <div className="foot_panel4">
+        <div className="pages">
+          <a>Conditions of use</a>
+          <a>Privacy Notice</a>
+          <a>Your ads Privacy Choices</a>
+        </div>
+        <div className="copyright"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
